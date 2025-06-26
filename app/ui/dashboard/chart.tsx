9c6f8ed19240530e6b7a26abf98de909f6399a82
Adriano <adriano.13lp@gmail.com>
2025-06-26 20:53:56 -0300

    feat: added renevue chart using recharts

---
+++ app/ui/dashboard/chart.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import {
+  Bar,
+  BarChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+import { Revenue } from '@/app/lib/definitions';
+import { use, useEffect } from 'react';
+
+export default function Chart({ revenue }: { revenue: Revenue[] }) {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={revenue}>
+        <XAxis
+          dataKey="month"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) =>
+            `$${new Intl.NumberFormat('en-US').format(value)}`
+          }
+        />
+        <Tooltip
+          contentStyle={{
+            backgroundColor: 'rgba(255, 255, 255, 0.8)',
+            border: '1px solid #ccc',
+            borderRadius: '0.5rem',
+          }}
+          labelStyle={{ fontWeight: 'bold' }}
+          formatter={(value: number) => [
+            new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            }).format(value),
+            'Revenue',
+          ]}
+        />
+        <Bar dataKey="revenue" fill="#3b82f6" radius={[4, 4, 0, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
